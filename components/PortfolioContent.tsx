aa813ae6f57a12cafbba5200521b9841660204b7
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-10-29 15:22:22 -0400

    added skills section component and rendered it with its respective ui components

--- components/PortfolioContent.tsx
+++ components/PortfolioContent.tsx
@@ -1,6 +1,7 @@
 import { AboutSection }  from "@/components/sections/AboutSection"
 import HeroSection from "@/components/sections/HeroSection"
 import { TestimonialsSection } from "./sections/TestimonialsSection"
+import { SkillsSection } from "./sections/SkillsSection"
 
 export default async function PortfolioContent() {
   return (
@@ -8,6 +9,7 @@ export default async function PortfolioContent() {
       <HeroSection />
       <AboutSection />
       <TestimonialsSection />
+      <SkillsSection />
     </>
   )
 }
