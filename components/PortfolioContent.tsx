7a473de12edd4a9631ba3655253b740a6b6ee530
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-10-29 14:34:38 -0400

    created testimonials section component and rendered it

--- components/PortfolioContent.tsx
+++ components/PortfolioContent.tsx
@@ -1,11 +1,13 @@
 import { AboutSection }  from "@/components/sections/AboutSection"
 import HeroSection from "@/components/sections/HeroSection"
+import { TestimonialsSection } from "./sections/TestimonialsSection"
 
 export default async function PortfolioContent() {
   return (
     <>
       <HeroSection />
       <AboutSection />
+      <TestimonialsSection />
     </>
   )
 }
